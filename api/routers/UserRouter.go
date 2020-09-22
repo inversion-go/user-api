a40338f68dbe5b866911a55095a4994d3381d743
Manuel alejandro alcala <alejoab12@hotmail.com>
2020-09-22 12:12:43 -0500

    fuentes base2

--- api/routers/UserRouter.go
+++ api/routers/UserRouter.go
@@ -1,7 +1,6 @@
 package routers
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/inversion-go/alejoab12/user-api/api/handler"
 	"log"
@@ -9,8 +8,7 @@ import (
 )
 
 func RouterUserHandler() {
-	fmt.Println("Entro a esta cagada")
 	userRouter := mux.NewRouter().StrictSlash(true)
-	userRouter.HandleFunc("/user/byId", handler.UserById).Methods("GET")
+	userRouter.HandleFunc("/user/findById", handler.UserById).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8082", userRouter))
 }
